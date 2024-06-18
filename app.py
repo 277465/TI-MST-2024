111e5c4c48fe045ae21e6d054326ad48aba0f94f
nikodemdrelak <163131906+nikodemdrelak@users.noreply.github.com>
2024-06-18 23:13:59 +0200

    zmiana wyglądu, dodanie przykładów
    Zmiana wyglądu strony (zmiana kolorów na ciemne, większe zaokrąglenia rogów), dodanie przykładów w dziedzinie i równaniach

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ import base64
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/project')
 def project():
     return render_template('project.html')
+
 
 @app.route('/demo', methods=['GET', 'POST'])
 def demo():
@@ -37,41 +40,51 @@ def demo():
             pass
     return render_template('demo.html', image=image)
 
+
 @app.route('/authors')
 def authors():
     return render_template('authors.html')
+
 
 @app.route('/introduction')
 def introduction():
     return render_template('introduction.html')
 
+
 @app.route('/inverse_proportion')
 def inverse_proportion():
     return render_template('inverse_proportion.html')
+
 
 @app.route('/inverse_proportion_plot')
 def inverse_proportion_plot():
     return render_template('inverse_proportion_plot.html')
 
+
 @app.route('/homographic_function')
 def homographic_function():
     return render_template('homographic_function.html')
+
 
 @app.route('/definition')
 def definition():
     return render_template('definition.html')
 
+
 @app.route('/domain')
 def domain():
     return render_template('domain.html')
+
 
 @app.route('/equations')
 def equations():
     return render_template('equations.html')
 
+
 @app.route('/inequalities')
 def inequalities():
     return render_template('inequalities.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
